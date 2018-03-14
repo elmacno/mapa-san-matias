35bbea2c569579237b0a4da3bb33d36adc8050fb
Matias Calcagno <elmacno@gmail.com>
2018-03-14 11:26:00 -0300

    Added material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
@@ -16,7 +20,17 @@ import { MapboxService } from './mapbox.service';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatGridListModule,
+    BrowserAnimationsModule
+  ],
+  exports: [
+    MatToolbarModule,
+    MatSidenavModule,
+    MatGridListModule,
+    BrowserAnimationsModule
   ],
   providers: [
     MapboxService
